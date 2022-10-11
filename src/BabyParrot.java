91e3017af6d36b84576f2bdd6be8b1779a10d208
unknown <BT_3W2_31@BKTECH.LOCAL>
2022-10-11 12:31:25 -0400

    Initial commit

---
+++ src/BabyParrot.java
@@ -0,0 +1,31 @@
+public class BabyParrot
+{
+    private String species;
+    private int age;
+    private String name;
+    private double weight;
+
+    public BabyParrot(String species, int age, double weight)
+    {
+        this.species = species;
+        this.age = age;
+        this.weight = weight;
+        this.name = ""; // use a default value of the 'empty string'
+    }
+
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+
+    // note this method RETURNS a String
+    public String parrotInfo()
+    {
+        String str = "Species: " + species + "\n";
+        str += "Name: " + name + "\n";
+        str += "Age: " + age + "\n";
+        str += "Weight:" + weight;
+        return str;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        BabyParrot baby = new BabyParrot("Toucan", 3,4.3);
+        baby.setName("Lil' Beans");
+        String info = baby.parrotInfo(); // store returned string in variable
+        Student student = new Student("Abby", "Smith", 2023);
+        student.addTestScore(95);
+        student.addTestScore(98);
+        System.out.println(student.getStudentInfo());
+    }
+}
